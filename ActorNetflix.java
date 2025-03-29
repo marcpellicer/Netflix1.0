8f7debfc9ef22556e4c9e43c6ada8e6795546a85
Marc Pellicer Esbrí <mpelliceresbri6@gmail.com>
2025-03-29 22:42:54 +0100

    Create ActorNetflix.java

---
+++ ActorNetflix.java
@@ -0,0 +1,19 @@
+public class ActorNetflix {
+    private String nombre;
+
+    public ActorNetflix(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String toString() {
+        return nombre;
+    }
+}
